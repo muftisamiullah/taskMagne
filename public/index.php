a1cbe3acaf229e50f7dff8590efe8a89a8b97992
Sami Ullah <muftisami2010@gmail.com>
2018-12-03 23:24:51 +0530

    initial commit

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+require 'templates/header.php';
+?>
+
+
+<p>
+    askdhkashdhakhd
+    dashdkahkdhskh
+</p>
+
+
+
+<?php
+require 'templates/footer.php';
+?>
+    
+
+
+
+
+
+
+
+
---
+++ public/templates/footer.php
@@ -0,0 +1,24 @@
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="contact.php">Contact Us<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About Us</a>
+    </li>
+    </ul>
+    <footer>
+  <p>Designed by: Sami Ullah</p>
+  <p>Contact information: <a href="http://muftisamiullah.co.nf/">http://muftisamiullah.com</a>.</p>
+</footer>
+  </div>
+</nav>
+</body>
+    </html>

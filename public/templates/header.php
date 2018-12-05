3ee074016ac44fb619d38ae990ffb47c4b51254f
Sami Ullah <muftisami2010@gmail.com>
2018-12-05 21:58:32 +0530

    final commit

--- public/templates/header.php
+++ public/templates/header.php
@@ -7,18 +7,21 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-        <title>Maqne Q Advertising</title>
+        <title>MaqneQ Advertising</title>
                 <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
   </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.php">Magne Q Advertising</a>
+      <a class="navbar-brand" href="index.php">MagneQ Advertising</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="nav navbar-nav mr-auto pt-1">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link" href="blogs.php">Blogs</a>
               </li>

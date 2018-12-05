1ef66ced751ac0c1512b8e2a4416bc0d7b37dcf4
Sami Ullah <muftisami2010@gmail.com>
2018-12-05 13:36:31 +0530

    solved error with bootstrap nav mobile

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,18 +1,22 @@
-<div class="footer"><nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="about.php">About Us</a>
-      </li>
-    </ul>
-    <footer>
-      <p>Designed by: Sami Ullah</p>
-      <p>Contact information: <a href="http://muftisamiullah.co.nf/">http://muftisamiullah.com</a>.</p>
-    </footer>
-  </div>
-</nav>
+<div class="footer">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About Us</a>
+        </li>
+      </ul>
+
+        <footer>
+          <p>Designed by: Sami Ullah</p>
+          <p>Contact information: <a href="http://muftisamiullah.co.nf/">http://muftisamiullah.com</a>.</p>
+        </footer>
+        
+    </div>
+  </nav>
+</div>
 </body>
 </html>
--- public/templates/header.php
+++ public/templates/header.php
@@ -1,28 +1,29 @@
-<html>
-<!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
         <title>Maqne Q Advertising</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css" type="text/css">
-        <!-- <script src="main.js"></script> -->
-</head>
-  <body>
+                <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
+  </head>
+<body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="index.php">Magne Q Advertising</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="nav navbar-nav mr-auto pt-1">
               <li class="nav-item">
-                <a class="nav-link single" href="blogs.php">Blogs</a>
+                <a class="nav-link" href="blogs.php">Blogs</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link single" href="services.php">Services</a>
+                <a class="nav-link" href="services.php">Services</a>
               </li>
             </ul>
          </div>
